79956027a711c612e282067a2d90500dc8e83522
Arma Burton <arma@armaburton.com>
2024-07-29 15:08:07 -0700

    fetch from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts(){
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if(!response.ok){
+          throw new Error('Network not available');
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Fetch API Example</h1>
       </header>
+      <div className="posts">
+        {posts.map(post => (
+          <div className="post">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
